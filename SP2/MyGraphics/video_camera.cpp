26c194a1f50e6c5b388b7b171c13c41b6e848a1f
FrancisWWL <sword_is_my_life@hotmail.com>
2016-02-19 16:43:04 +0800

    Setting Up CCTV header and CPP file

---
+++ SP2/MyGraphics/video_camera.cpp
@@ -0,0 +1,11 @@
+#include "video_camera.h"
+
+
+video_camera::video_camera()
+{
+}
+
+
+video_camera::~video_camera()
+{
+}
---
+++ SP2/MyGraphics/video_camera.h
@@ -0,0 +1,8 @@
+#pragma once
+class video_camera
+{
+public:
+	video_camera();
+	~video_camera();
+};
+
